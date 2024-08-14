ef27ab375337aad0ca62c83b68ab5d124e836dde
edgar <edgarscirulis80@inbox.lv>
2024-08-14 21:24:58 +0300

    Text to Morse code - initial

---
+++ main.py
@@ -0,0 +1,36 @@
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def converting_to_morses(sentence):
+    space_letters = '  '
+    space_words = '    '
+    cipher = ''
+    for letter in sentence:
+        if letter !=' ':
+            cipher += MORSE_CODE_DICT[letter.upper()]
+            cipher += space_letters
+        else:
+            cipher += space_words
+    return cipher
+
+source_text = input("Input text for converting to Morses code: ")
+
+coded_text = converting_to_morses(source_text)
+
+print(coded_text)
+
+
